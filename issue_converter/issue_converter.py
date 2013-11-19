c8a21b8779a62e7b4ad656c444c501972a3757b0
Claudio Kirchhoff <claudio.kirchhoff@gmail.com>
2013-11-19 15:36:50 +0100

    use csvreader

--- issue_converter/issue_converter.py
+++ issue_converter/issue_converter.py
@@ -11,6 +11,7 @@
 # generiere output filename aus inputfilename+'.out'
 
 import sys
+import csv
 
 if (len(sys.argv) == 1):
     filename = 'issues.csv'
@@ -22,42 +23,50 @@ else:
 
 newfilename = filename + '.out'
 
-try:
-    lines = open(filename, 'r').readlines()
-except IOError:
-    print "There is no file named '" + filename + "' in this directory."
-    sys.exit(0)
+
+#    for row in csvreader:
+#        print ', '.join(row)
+
+
+# try:
+#     lines = open(filename, 'r').readlines()
+# except IOError:
+#     print "There is no file named '" + filename + "' in this directory."
+#     sys.exit(0)
 
 newfile = open(newfilename, 'w')
 
 cnt = 0
 
-for line in lines:
-    items = line.strip().split(',')
-    if cnt == 0:
-        url = "Url"
-    else:
-        url = 'https://projects.syslab.com/issues/' + items[0]
+with open(filename, 'rb') as csvfile:
+    csvreader = csv.reader(csvfile, delimiter=',', quotechar='"')
+    for line in csvreader:
+        items = line #.strip().split(',')
+        if cnt == 0:
+            url = "Url"
+        else:
+            url = 'https://projects.syslab.com/issues/' + items[0]
 
-    newitems = [url] + items
-    if (newitems[4]=='Low (P4)'):
-        newitems[4]='-1'
-    elif (newitems[4]=='Normal (P3)'):
-        newitems[4]='0'
-    elif (newitems[4] in ('High (P2)','Very High (P1)')):
-        newitems[4]='1'
-    else:
-        newitems[4]=newitems[4]
+        newitems = [url] + items
+        if (newitems[4]=='Low (P4)'):
+            newitems[4]='-1'
+        elif (newitems[4]=='Normal (P3)'):
+            newitems[4]='0'
+        elif (newitems[4] in ('High (P2)','Very High (P1)')):
+            newitems[4]='1'
+        else:
+            newitems[4]=newitems[4]
 
-    # drop a few columns
-    del newitems[11]  # position
-    del newitems[7]  # Updated
-    del newitems[3]  # Status
-    del newitems[2]  # Tracker
+        # drop a few columns
+        #del newitems[11]  # position
+        del newitems[7]  # Updated
+        del newitems[3]  # Status
+        del newitems[2]  # Tracker
 
-    newline = ','.join(newitems)
-    newfile.write(newline+'\n')
-    cnt += 1
+        newline = ','.join(newitems)
+        newfile.write(newline+'\n')
+        cnt += 1
+
 
 print "Done. File converted. Output in %s" % newfilename
 newfile.close()
